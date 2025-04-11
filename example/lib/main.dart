c4238cdeba4fec5bb01b1779a39dc32b31a5e42a
medvedev <maxxon2235@gmail.com>
2025-04-11 14:34:30 +0300

    background

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:hello/hello.dart';
 
 void main() {
@@ -54,10 +58,88 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            ElevatedButton(
+              child: Text('Start Service'),
+              onPressed: () async {
+                final service = FlutterBackgroundService();
+                await service.startService().then(
+                      (value) => log('running service'),
+                    );
+              },
+            ),
+            ElevatedButton(
+              child: Text('get platform version'),
+              onPressed: () async {
+                final service = FlutterBackgroundService();
+                service.invoke('init');
+              },
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: false,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: false,
+
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: null,
+    ),
+  );
+}
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  return true;
+}
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) async {
+      await service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) async {
+      await service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) async {
+    await service.stopSelf();
+  });
+  service.on('init').listen((event) async {
+    final hello = Hello();
+    log('get perm ${await hello.getPlatformVersion()}');
+  });
+  service.on('start').listen((event) async {});
+  service.on('stop').listen((event) async {});
 }
